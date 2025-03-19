5c5a9fb99aa2f0d230fbfead86a0ad205763bd9c
Aloysius Goh <aloysiusgohkw@gmail.com>
2025-03-20 01:25:39 +0800

    refactor

--- telegram_gemma_3_bot.py
+++ telegram_gemma_3_bot.py
@@ -6,69 +6,60 @@ from ollama import AsyncClient
 import asyncio
 
 TOKEN = os.environ.get('TELEGRAM_GEMMA_3_BOT_TOKEN')
+MODEL = 'gemma3'
+MAX_MSG_LEN = 600
+MAX_CHUNK_LEN = 10
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
-def check_sentence_end(sentence):
-    stop = False
-    stop_check = ['.', '?', '!', '\n']
-    for s in stop_check:
-        if s in sentence:
-            stop = True
-            
-    return stop
-
-async def ask(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    msg = ' '.join(context.args)
-    gemma_msg = {
-      'role': 'user', 
-      'content': msg
-    }
-    
-    msg_sent = await context.bot.send_message(
-            chat_id=update.effective_chat.id, 
-            text="loading...",
-            parse_mode="Markdown"
-    )
-    
-    sentence = []
-    async for part in await AsyncClient().chat(model='gemma3', messages=[gemma_msg], stream=True):
-        sentence.append(part['message']['content'])
-        
-        if len(sentence) >= 600:
-            break
-        
-        if len(sentence) % 10 == 0:
-            sent = ''.join(sentence).replace('*', '-').replace('--', '*')
-        
-            try:
-                await context.bot.editMessageText(
-                    chat_id=update.effective_chat.id,
-                    message_id=msg_sent.message_id, 
-                    text=sent,
-                    parse_mode="Markdown"
-                )
-            except Exception as e:
-                print(e)
-                
-    try:
-        await context.bot.editMessageText(
-            chat_id=update.effective_chat.id,
-            message_id=msg_sent.message_id, 
-            text=''.join(sentence).replace('*', '-').replace('--', '*'),
-            parse_mode="Markdown"
-        )
-    except Exception as e:
-        print(e)
-        
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
         chat_id=update.effective_chat.id, 
         text="Greetings, I'm Artoria Pendragon, King of Knights. Pleased to make your acquaintance!"
     )
+
+async def ask(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    def to_msg(list):
+        return ''.join(list).replace('*', '-').replace('--', '*')
+    
+    async def send_msg(msg, msg_list):
+        try:
+            await context.bot.editMessageText(
+                chat_id=update.effective_chat.id,
+                message_id=msg.message_id, 
+                text=to_msg(msg_list),
+                parse_mode="Markdown"
+            )
+        except Exception as e:
+            print(e)
+    
+    msg = await context.bot.send_message(
+            chat_id=update.effective_chat.id, 
+            text="loading...",
+            parse_mode="Markdown"
+    )
+    msg_list = []
+    
+    async for chunk in await AsyncClient().chat(
+        model=MODEL,
+        messages=[{
+            'role': 'user', 
+            'content': ' '.join(context.args)
+        }],
+        stream=True
+    ):
+        msg_list.append(chunk['message']['content'])
+        
+        if len(msg_list) >= MAX_MSG_LEN:
+            break
+        
+        if len(msg_list) % MAX_CHUNK_LEN == 0:   
+            send_msg(msg, msg_list)
+                
+    send_msg(msg, msg_list)
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(TOKEN).build()
